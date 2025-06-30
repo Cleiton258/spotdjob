7876ed76020c205f9230014faf37418ff592fa11
Cleiton258 <cleitonernestocumbane0101@gmail.com>
2025-06-30 17:26:33 -0400

    views e urls com FBV

--- meu_lista_de_tarefas/urls.py
+++ meu_lista_de_tarefas/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from tarefas.views import  customLoginView, custom_logout
+from tarefas.views import  customLoginView #custom_logout
 from django.shortcuts import redirect
 from tarefas.views import RegisterView, home, adicionar_vaga, vaga_detalhes, sobre
 from django.contrib.auth import views as auth_views
